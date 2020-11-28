812179d5227735bee119e742caa7b3fe43ec5fbd
Иван <kleimenov416@gmail.com>
2020-11-27 19:34:23 -0500

    add JS code, add likes ligic (in this case fase masks) when you click on face mask, this change color

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let heart = document.querySelector('.heart');
+let hearts = document.querySelectorAll('.heart');
 
+for (let heart of hearts) {
+  heart.onclick = () => {
+    heart.classList.toggle('added');
+  };
+}
+
+/*
 heart.onclick = () => {
   heart.classList.toggle('added');
 };
+*/
