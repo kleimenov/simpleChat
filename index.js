9d5b1b0c68d375527d7ad6c415504bfe199ff579
Иван <kleimenov416@gmail.com>
2020-11-28 14:32:11 -0500

    add likes counter

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let heart = document.querySelector('.heart');
 let hearts = document.querySelectorAll('.heart');
-let likesCounter = document.querySelector('.likes-number');
+//let likesCounter = document.querySelector('.likes-number');
 
 
 for (let heart of hearts) {
   heart.onclick = () => {
+    let likesCounter = heart.querySelector('.likes-number');
     if (heart.classList.contains('added')) {
       likesCounter.textContent--;  
     } else {
-      likesCounter.textContent++
+      likesCounter.textContent++;
     }
     heart.classList.toggle('added');
   };
