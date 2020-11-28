31943e2aecef9f3955c6da0e75839b5e479a238e
Иван <kleimenov416@gmail.com>
2020-11-28 14:10:43 -0500

    add likes counter

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 let heart = document.querySelector('.heart');
 let hearts = document.querySelectorAll('.heart');
+let likesCounter = document.querySelector('.likes-number');
+
 
 for (let heart of hearts) {
   heart.onclick = () => {
+    if (heart.classList.contains('added')) {
+      likesCounter.textContent--;  
+    } else {
+      likesCounter.textContent++
+    }
     heart.classList.toggle('added');
   };
 }
 
-/*
-heart.onclick = () => {
-  heart.classList.toggle('added');
-};
-*/
+
